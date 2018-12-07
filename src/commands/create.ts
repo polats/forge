97adb45bc4e0957d26b42320661c94c4c19e2788
Paul Gadi <paulgadi@gmail.com>
2018-12-08 02:08:11 +0800

    compile fixes

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -21,29 +21,12 @@ hello world from ./src/hello.ts!
   static args = [{name: 'file'}]
 
   async run() {
-    const {args, flags} = this.parse(Create)
-    const chalk       = require('chalk');
-    const clear       = require('clear');
-    const figlet      = require('figlet');
-    const files       = require('../lib/files');
-
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from .\\src\\commands\\create.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
-
-    if (!files.fileExists('.env')) {
-      console.log(chalk.yellow("Environment file not found, running 'forge init'."));
-      process.exit();
-    }
-
     const Web3 = require('web3')
-    const rpcURL = process.env.RPC_SERVER + process.env.INFURA_API_KEY;
+    const rpcURL = (process.env.RPC_SERVER as string) + process.env.INFURA_API_KEY;
     const web3 = new Web3(rpcURL);
     const address = process.env.USER_ADDRESS;
 
-    web3.eth.getBalance(address, (err, wei) => {
+    web3.eth.getBalance(address, (err : Error, wei: Number) => {
       var balance = web3.utils.fromWei(wei, 'ether')
       console.log("Balance - " + address + ": " + balance + "ETH");
     })
